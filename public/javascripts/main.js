fa7f3a2fd0b4c8196de072ea9066de853181444f
Qasim Iqbal <me@qas.im>
2014-06-21 14:26:17 -0400

    Google Maps API tests

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -9,4 +9,20 @@ $(document).ready(function() {
 		socket.emit('go', { courseList: courseList });
 	});
 
+	socket.on('adjacent classes', function(data) {
+		console.log(data);
+	});
+
+	var service = new google.maps.DistanceMatrixService();
+
+	service.getDistanceMatrix({
+    origins: ["100  St. George Street, M5S 3G3"],
+    destinations: ["93  Charles St. West, M5S 1K9"],
+    travelMode: google.maps.TravelMode.WALKING
+  }, function(response, status) {
+		if (status == google.maps.DistanceMatrixStatus.OK) {
+			console.log(response.rows[0].elements[0].duration);
+		}
+	});
+
 });
